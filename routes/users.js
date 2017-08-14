54e929dc4c5acff282eb7337ec6c4f3454f93440
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-14 10:46:16 -0400

    adds users to route and tempelate

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('users', {
+    username: "mirvine",
+    password: "1234"
+  });
 });
 
 module.exports = router;
