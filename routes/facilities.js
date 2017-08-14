b83227f5657c56732fcc60395ca56b4438c71c2c
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-14 10:46:01 -0400

    adds facilities to routes and templates

---
+++ routes/facilities.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+  res.render('users', {
+    one: "house",
+    two: "building"
+  });
+});
+
+module.exports = router;
